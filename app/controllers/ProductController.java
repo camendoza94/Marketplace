4e7b4b2a3e87c609a8487a121cabbf801c4f397f
CAMILO ANDRES MENDOZA ARRIETA <ca.mendoza968@sis.virtual.uniandes.edu.co>
2016-08-20 16:59:06 -0500

    Initial commit

---
+++ app/controllers/ProductController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import dispatchers.AkkaDispatcher;
+import java.util.concurrent.CompletableFuture;
+import static play.libs.Json.toJson;
+import models.ProductEntity;
+import akka.dispatch.MessageDispatcher;
+import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class ProductController extends Controller {
+    public CompletionStage<Result> getProducts() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return ProductEntity.FINDER.all();
+                        }
+                        , jdbcDispatcher)
+                .thenApply(
+                        productEntities -> {
+                            return ok(toJson(productEntities));
+                        }
+                );
+    }
+
+    public CompletionStage<Result> createProduct(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nProduct = request().body().asJson();
+        ProductEntity product = Json.fromJson( nProduct , ProductEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    product.save();
+                    return product;
+                }
+        ).thenApply(
+                productEntity -> {
+                    return ok(Json.toJson(productEntity));
+                }
+        );
+    }
+}
